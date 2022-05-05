e9da966c4bbd8a151a6fceddb39046c1f6a7adbd
Divine Niiquaye Ibok <divineibok@gmail.com>
2022-05-05 16:26:22 +0000

    Fixed minor whitespace issue in the `app.tsx` file

--- resources/static/app.tsx
+++ resources/static/app.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, BrowserRouter as Router  } from 'react-router-dom';
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
 import Layout from './components/layout';
 import HomePage from './pages/home';
 
